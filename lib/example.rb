21ef1c1f047e2df136c2c00be14a8a593656fcc0
Megan Christine McClure <mcclme02@luther.edu>
2020-03-02 14:57:03 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,22 @@
 # Use this space to code along with the readme
+def say_hello_world_ten_times
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+say_hello_world_ten_times
+
+def greeting
+  puts "Hello World"
+end
+
+greeting
