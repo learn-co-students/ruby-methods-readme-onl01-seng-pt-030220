c538c862de6e7651ebf40ac0160555757c1ef8cb
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-03 05:14:36 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -11,3 +11,28 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts
+
+def say_hello_world_ten_times
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
