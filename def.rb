31bb7679b0ccd55ccecb135f4f04696fd7be2c8e
beloved-scraper-9319 <dsjr1284@gmail.com>
2020-03-04 02:20:24 +0000

    Done.

---
+++ def.rb
@@ -0,0 +1,15 @@
+def say_hello_world_ten_times
+  pharse = "Hello World"
+  puts pharse
+  puts pharse
+  puts pharse
+  puts pharse
+  puts pharse
+  puts pharse
+  puts pharse
+  puts pharse
+  puts pharse
+  puts pharse
+  puts pharse 
+end 
+say_hello_world_ten_times
--- lib/example.rb
+++ lib/example.rb
@@ -11,3 +11,19 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+def say_go_falcons_ten_times
+  phrase="Go say_go_falcons_ten_times"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
+say_go_falcons_ten_times
+
+  
